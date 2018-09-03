de52c6a0007a9b7e9fda212890ea24736b90dc3e
Ariful Islam <arif98741@gmail.com>
2018-09-03 17:30:02 +0600

    Database Updated

--- database/dts.sql
+++ database/dts.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 10, 2017 at 10:21 AM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.28
+-- Generation Time: Sep 03, 2018 at 01:28 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -93,9 +95,9 @@ CREATE TABLE `tbl_customer` (
 
 INSERT INTO `tbl_customer` (`serial`, `customer_id`, `customer_name`, `address`, `contact_no`, `email`, `opening_balance`, `remark`, `discount`, `updateby`) VALUES
 (101, '14', 'Arif', 'Tangail', '012-25-6538', 'arifsofg@gmail.com', '0', 'Good', 250, 1),
-(107, '125', 'Kamrul Islam', 'Kalihati', '02-894343-454', 'kamrul@gmail.com', '1300', 'Good', 1200, 0),
-(122, '123', 'Jhon Doe', 'Usa', '01923-xxxxxx', 'jhon@gmail.com', '2560', 'Good', 1200, 1),
-(126, '127', 'Shamim AL-Deen', 'Ghatail, Tangail', '01738-298666', 'shamimaldeen@gmail.com', '5620', 'Good and Outstanding', 250, 0);
+(127, '145', 'Kristofer Alen', 'New York City, CA', '+12-28236-GD', 'kristofer@gmail.com', '1500', 'Better', 150, 0),
+(128, '146', 'Nadhan Hough', 'California, CA. Street Road 123', '+15-2852-4FG', 'hogh@gmail.com', '500', 'WOW', 1400, 0),
+(129, '147', 'Kristen More', 'WD, US 14/343', '+14-215DFDE', 'more@gmail.com', '1250', 'Not Bad', 1470, 0);
 
 -- --------------------------------------------------------
 
@@ -142,6 +144,15 @@ CREATE TABLE `tbl_invoice` (
   `updateby` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tbl_invoice`
+--
+
+INSERT INTO `tbl_invoice` (`serial`, `invoice_number`, `supplier_id`, `quantity`, `carton`, `piece`, `purchase`, `subtotal`, `total`, `date`, `status`, `updateby`) VALUES
+(1, '45875055', 5, 22, 10, 44, 21, 268, 268, '2018-09-13', NULL, 0),
+(2, '145870000', 6, 8, 6, 19, 8, 73, 73, '2018-09-03', NULL, 0),
+(3, '14587000', 3, 23, 9, 33, 24, 129, 129, '2018-09-02', NULL, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -158,6 +169,24 @@ CREATE TABLE `tbl_invoice_products` (
   `purchase` int(11) NOT NULL,
   `subtotal` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_invoice_products`
+--
+
+INSERT INTO `tbl_invoice_products` (`serial_no`, `invoice_id`, `product_id`, `quantity`, `carton`, `piece`, `purchase`, `subtotal`) VALUES
+(1, '45875055', '1215', 12, 1, 12, 12, 144),
+(2, '45875055', '1210', 2, 2, 4, 2, 8),
+(3, '45875055', '1211', 4, 5, 20, 5, 100),
+(4, '45875055', '1214', 4, 2, 8, 2, 16),
+(5, '145870000', '1212', 1, 1, 1, 1, 1),
+(6, '145870000', '1213', 3, 2, 6, 2, 12),
+(7, '145870000', '1209', 4, 3, 12, 5, 60),
+(8, '14587000', '1216', 12, 1, 12, 1, 12),
+(9, '14587000', '1214', 2, 2, 4, 2, 8),
+(10, '14587000', '1213', 4, 1, 4, 5, 20),
+(11, '14587000', '1212', 3, 3, 9, 5, 45),
+(12, '14587000', '1215', 2, 2, 4, 11, 44);
 
 -- --------------------------------------------------------
 
@@ -212,6 +241,9 @@ CREATE TABLE `tbl_sell` (
   `seller` int(11) NOT NULL,
   `sub_total` float NOT NULL,
   `grand_total` float NOT NULL,
+  `dlcharge` float(10,2) NOT NULL DEFAULT '0.00',
+  `discount` float(10,2) NOT NULL DEFAULT '0.00',
+  `vat` float(8,2) NOT NULL DEFAULT '0.00',
   `paid` float NOT NULL,
   `due` float NOT NULL,
   `status` float NOT NULL,
@@ -223,9 +255,11 @@ CREATE TABLE `tbl_sell` (
 -- Dumping data for table `tbl_sell`
 --
 
-INSERT INTO `tbl_sell` (`serial`, `sell_id`, `customer_id`, `seller`, `sub_total`, `grand_total`, `paid`, `due`, `status`, `date`, `updateby`) VALUES
-(35, '1020171000000', 121, 1, 84505.8, 411259, 352511, 58747.9, 0, '2017-10-24 22:38:33', 1),
-(52, '1020171000001', 126, 1, 10298.8, 2433.48, 2500, 0, 0, '2017-10-29 17:34:50', 1);
+INSERT INTO `tbl_sell` (`serial`, `sell_id`, `customer_id`, `seller`, `sub_total`, `grand_total`, `dlcharge`, `discount`, `vat`, `paid`, `due`, `status`, `date`, `updateby`) VALUES
+(1, '0920181', 14, 1, 135, 84.1, 0.00, 0.00, 0.00, 200, 0, 0, '2018-09-03 10:24:57', 1),
+(2, '0920182', 123, 1, 2072, 465.39, 0.00, 0.00, 0.00, 2, 463.39, 0, '2018-09-03 10:27:53', 1),
+(4, '0920183', 14, 1, 1774.56, 341721, 0.00, 0.00, 0.00, 300000, 41720.7, 0, '2018-09-03 11:16:31', 1),
+(5, '0920184', 14, 1, 4453.7, 573.61, 0.00, 0.00, 0.00, 500, 73.61, 0, '2018-09-03 11:20:00', 1);
 
 -- --------------------------------------------------------
 
@@ -251,8 +285,17 @@ CREATE TABLE `tbl_sell_products` (
 --
 
 INSERT INTO `tbl_sell_products` (`serial_no`, `sell_id`, `customer_id`, `product_id`, `quantity`, `product_piece`, `unit_price`, `discount`, `subtotal`, `status`) VALUES
-(187, '1020171000000', 121, 1211, 45, 54, 30618, 'null', 14084.3, 1),
-(418, '1020171000001', 126, 1215, 21, 12, 6240, 'null', 6115.2, 1);
+(1, '0920181', 14, 1212, 25, 2, 150, 'null', 135, 1),
+(4, '0920183', 14, 1212, 3, 3, 225, 'null', 112.5, 1),
+(5, '0920183', 14, 1216, 3, 3, 672, 'null', 658.56, 1),
+(6, '0920183', 14, 1213, 3, 3, 447, 'null', 223.5, 1),
+(7, '0920183', 14, 1215, 3, 3, 1560, 'null', 780, 1),
+(8, '0920184', 14, 1212, 25, 2, 150, 'null', 75, 1),
+(9, '0920184', 14, 1210, 25, 2, 2680, 'null', 2010, 1),
+(10, '0920184', 14, 1216, 25, 2, 448, 'null', 224, 1),
+(11, '0920184', 14, 1208, 25, 2, 974, 'null', 779.2, 1),
+(12, '0920184', 14, 1215, 25, 3, 1560, 'null', 546, 1),
+(13, '0920184', 14, 1213, 25, 11, 1639, 'null', 819.5, 1);
 
 -- --------------------------------------------------------
 
@@ -278,9 +321,9 @@ CREATE TABLE `tbl_supplier` (
 --
 
 INSERT INTO `tbl_supplier` (`serial`, `supplier_id`, `supplier_name`, `address`, `contact_no`, `email`, `contact_person`, `opening_balance`, `remark`, `updateby`) VALUES
-(68, '2', 'Shamim Al-Deen', 'Ghatail', '01738298666', 'shamimaldeen@gmail.com', 'Shamim', 13000, 'Wow', 1),
+(68, '2', 'Shamim Al-Deen', 'Ghatail', '01738-RTRTRT', 'shamimaldeen@gmail.com', 'Shamim', 13000, 'Wow', 1),
 (69, '3', 'Roudro Mehedi', 'Sherpur', '019XX-YYYYYY', 'roudro.m@gmail.com', 'Roudro', 13000, 'Wow', 1),
-(74, '5', 'Idris Hossain', 'Sherpur', '01922-552222', 'idris@gmail.com', 'Contact Person', 25698, 'Better', 1),
+(74, '5', 'Idris Hossain', 'Sherpur', '01922-PPPPPP', 'idris@gmail.com', 'Contact Person', 25698, 'Better', 1),
 (75, '6', 'Raisul Islam', 'Narayangonj', '01548-xxxxxx', 'raisul@gmail.com', 'Raisul', 2540, 'Outstanding', 1);
 
 -- --------------------------------------------------------
@@ -415,61 +458,74 @@ ALTER TABLE `tbl_user`
 --
 ALTER TABLE `tbl_brand`
   MODIFY `brandid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `tbl_color`
 --
 ALTER TABLE `tbl_color`
   MODIFY `colorid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
 --
 -- AUTO_INCREMENT for table `tbl_customer`
 --
 ALTER TABLE `tbl_customer`
-  MODIFY `serial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=127;
+  MODIFY `serial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=130;
+
 --
 -- AUTO_INCREMENT for table `tbl_group`
 --
 ALTER TABLE `tbl_group`
   MODIFY `groupid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
 -- AUTO_INCREMENT for table `tbl_invoice`
 --
 ALTER TABLE `tbl_invoice`
-  MODIFY `serial` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `serial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `tbl_invoice_products`
 --
 ALTER TABLE `tbl_invoice_products`
-  MODIFY `serial_no` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `serial_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
 -- AUTO_INCREMENT for table `tbl_product`
 --
 ALTER TABLE `tbl_product`
   MODIFY `serial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
+
 --
 -- AUTO_INCREMENT for table `tbl_sell`
 --
 ALTER TABLE `tbl_sell`
-  MODIFY `serial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+  MODIFY `serial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT for table `tbl_sell_products`
 --
 ALTER TABLE `tbl_sell_products`
-  MODIFY `serial_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=419;
+  MODIFY `serial_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
 --
 -- AUTO_INCREMENT for table `tbl_supplier`
 --
 ALTER TABLE `tbl_supplier`
   MODIFY `serial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
+
 --
 -- AUTO_INCREMENT for table `tbl_type`
 --
 ALTER TABLE `tbl_type`
   MODIFY `typeid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
   MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
